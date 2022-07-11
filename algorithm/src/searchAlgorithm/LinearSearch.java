cc144939f699c23095f41dd2c149c761526be59c
mustafaguler3 <mustafa.glr@hotmail.com>
2022-07-11 22:50:04 +0300

    added linear search with recursion

--- algorithm/src/searchAlgorithm/LinearSearch.java
+++ algorithm/src/searchAlgorithm/LinearSearch.java
@@ -12,4 +12,14 @@ public class LinearSearch {
         return -1;
     }
 
+    public int linear_search_recursion(int[] array,int item,int index){
+
+        // search miss
+        if(index >= array.length) return -1;
+
+        // base case when we find the item
+        if(array[index] == item) return index;
+
+        return linear_search_recursion(array,item,index + 1);
+    }
 }
