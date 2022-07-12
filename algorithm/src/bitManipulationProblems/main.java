a9c0e5640512f9add945bb83964b916c8c86bbd7
mustafaguler3 <mustafa.glr@hotmail.com>
2022-07-12 13:29:46 +0300

    added russian peasant problem

--- algorithm/src/bitManipulationProblems/main.java
+++ algorithm/src/bitManipulationProblems/main.java
@@ -37,4 +37,57 @@ public class main {
         }
         return false;
     }
+
+    //if 'b' is even
+    // a x b = (2a) x (b/2)
+
+    // if 'b' is not even (odd)
+    // a x b = (2a) x (b/2) + a
+
+    // 5 x 7 = 10 x 3 + 5
+
+    public static int multiply(int a, int b){
+
+        int result = 0;
+
+        while (b > 0){
+            // if 'b' becomes odd (XOR operator decrements the value by 1 for odd numbers)
+            // we want to skip when 'b' is even
+            if ((b ^ 1) == b - 1){
+                result = result + a;
+            }
+            // double the first number
+            a = a << 1;
+            // halve the second number
+            b = b >> 1;
+
+        }
+        return result;
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
